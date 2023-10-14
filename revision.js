86a8c3df9902928ef2723c96c9dd3f6c47df1411
Jerinislam05 <jerinislam05@gmail.com>
2023-10-14 23:11:53 +0100

    revision

---
+++ revision.js
@@ -0,0 +1,16 @@
+// string type variable
+var bottleColor = 'yellow';
+// Number type variable
+var sunglassPrice = '120';
+// boolean type variable
+var isExpensive = true;
+
+// array
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper']; //0 diye sob index suru hoy
+
+var item3 = tableItems[3]; // 3 number index item pete
+tableItems[4] = 'notebook'; // index item set korte
+var penIndex = tableItems.indexOf('pen'); //index ta paite
+// push pop holo sesh er item add/remove korte eta use hoy
+
+// conditionals
