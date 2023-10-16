a5c82b66660236bc8c58f702c5504cc7c6d48f1d
Jerinislam05 <jerinislam05@gmail.com>
2023-10-16 23:34:25 +0100

    learn array-loop

---
+++ array-loop.js
@@ -0,0 +1,16 @@
+/* for(var i = 0; i < 10; i++) {
+    console.log(i);
+} */
+
+// target: display every element of an array
+/* var numbers = [45, 11, 77, 38, 83, 45, 68, 36, 78, 45];
+for(var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    console.log(number);
+} */
+
+var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
+for(var i = 0; i < items.length; i++) {
+    var item = items[i];
+    console.log(item);
+}
